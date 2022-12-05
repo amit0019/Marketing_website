64fdf2a5eef85472b45f492e7fbea08f335925eb
Amit Singh <112752003+amit0019@users.noreply.github.com>
2022-12-05 20:34:03 +0530

    Add files via upload

---
+++ Finalproject.html/mainpage.js
@@ -0,0 +1,26 @@
+let Navbar = document.querySelector('.navbar');
+
+
+document.querySelector('#bars-btn').onclick = () =>{
+    Navbar.classList.toggle('active');
+    SearchForm.classList.remove('active');
+    loginForm.classList.remove('active')
+}
+
+
+let SearchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    SearchForm.classList.toggle('active');
+    Navbar.classList.remove('active');
+    loginForm.classList.remove('active')
+}
+
+
+let loginForm = document.querySelector('.login-form');
+document.querySelector('#user-btn').onclick = () =>{
+    window.location.href = "loginpage.html", '_blank';
+    
+    
+
+}
